5a1d2189cc5e981593f15785b2babc199f1e4ada
Nick Breslin <nickbreslin@gmail.com>
2021-09-18 23:14:51 -0700

    Filtering commands working

--- src/components/TheSpeechManager.vue
+++ src/components/TheSpeechManager.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="card shadow mb-3 shadow">
     <div class="card-header h3 text-uppercase text-center">TRANSCRIPT</div>
-    <div class="card-body">
-      <div class="h1">{{ context }}</div>
+    <div class="card-body text-center">
+      <div class="h1">{{ command }}</div>
+      <div class="text-small">{{ transcript }}</div>
       <!--<div class="alert alert-warning">{{ confidence }}</div>-->
+      <button
+        v-show="!active"
+        class="btn btn-lg btn-primary text-uppercase"
+        @click="toggleRecognition()"
+      >
+        <i class="bi bi-mic-fill me-3"></i>Start
+      </button>
+      <button
+        v-show="active"
+        class="btn btn-lg btn-danger text-uppercase"
+        @click="toggleRecognition()"
+      >
+        <i class="bi bi-mic-mute-fill me-3"></i>Stop
+      </button>
     </div>
   </div>
 </template>
@@ -13,13 +28,51 @@ export default {
   name: "TheSpeechManager",
   data: function () {
     return {
-      context: "",
+      active: true,
+      transcript: "",
+      command: "",
       confidence: "",
     };
   },
   props: {},
   computed: {},
   methods: {
+    toggleRecognition() {
+      this.active = !this.active;
+
+      if (this.active) {
+        this.recognition.start();
+      } else {
+        this.recognition.stop();
+      }
+    },
+    validateTranscript(transcript) {
+      console.log(transcript);
+
+      this.command = "";
+
+      if (transcript.length < 2 || transcript.length > 3) {
+        return false;
+      }
+
+      let x = transcript[0];
+      let y = transcript.substring(1, transcript.length);
+
+      x = x.toUpperCase();
+
+      let xAxis = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+      let yAxis = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
+      if (!xAxis.includes(x)) {
+        return false;
+      }
+
+      if (!yAxis.includes(y)) {
+        return false;
+      }
+
+      this.command = `${x}${y}`;
+    },
     init() {
       const SpeechRecognition =
         SpeechRecognition || window.webkitSpeechRecognition;
@@ -29,17 +82,15 @@ export default {
         SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
 
       this.recognition = new SpeechRecognition();
-      const speechRecognitionList = new SpeechGrammarList();
-      //speechRecognitionList.addFromString(grammar, 1);
-      this.recognition.grammars = speechRecognitionList;
+      this.recognition.grammars = new SpeechGrammarList();
       this.recognition.continuous = false;
       this.recognition.lang = "en-US";
       this.recognition.interimResults = false;
       this.recognition.maxAlternatives = 1;
 
       this.recognition.onresult = (event) => {
-        const result = event.results[0][0].transcript;
-        this.context = result;
+        this.validateTranscript(event.results[0][0].transcript);
+        this.transcript = event.results[0][0].transcript;
         this.confidence = event.results[0][0].confidence;
       };
 
@@ -49,7 +100,9 @@ export default {
       };
       this.recognition.onend = () => {
         console.log("OnEnd");
-        this.recognition.start();
+        if (this.active) {
+          this.recognition.start();
+        }
       };
 
       this.recognition.onnomatch = (event) => {
